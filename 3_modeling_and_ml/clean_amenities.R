15ab00d6bed3ffb268d0b4318b0440261e4db05c
Colin Pawlowski <cpawlows@mit.edu>
2018-01-02 16:05:32 -0500

    fixed merge conflict in clean_amenities

--- 3_modeling_and_ml/clean_amenities.R
+++ 3_modeling_and_ml/clean_amenities.R
@@ -4,20 +4,12 @@ clean_amenities <- function(listings_df) {
     mutate(amenities = gsub("[{}]|\"|[()]|-", "", amenities)) %>%
     mutate(amenities = gsub(" |/", "_", amenities)) %>%
     mutate(amenities = gsub("24", "x24", amenities))
-  
-<<<<<<< HEAD
+
   # Then, split the strings by amenity and create new columns
   splitting <- strsplit(listings_df$amenities, ",")
   all_amenities <- Reduce(union, splitting)
   for (i in all_amenities) {
     listings_df[paste0("amenity_", i)] <- 
-=======
-  # Then, split the strings by amenity and create new column
-  splitting <- strsplit(listings_df$amenities, ",")
-  all_amenities <- Reduce(union, splitting)
-  for (i in all_amenities) {
-    listings_df[paste("amenity_", i, sep = "")] <- 
->>>>>>> 4cd374e2c4858a90aa6398913786cb0d84f9b3fa
       grepl(i, listings_df$amenities)
   }
   return(listings_df)
